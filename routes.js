9edb24b72ef427a134151275d7f5ee2498f76c34
Pradeep <pcngu1@gmail.com>
2018-11-04 19:30:10 +0530

    Added items and orders api

--- routes.js
+++ routes.js
@@ -40,6 +40,78 @@ router = app => {
 			response.send("User deleted!\n");
 		});
 	});
+
+	app.get("/items", (request, response) => {
+		pool.query("SELECT * FROM items", (error, result) => {
+			if(error){
+				return console.log(error);
+			}
+			response.send(result);
+		});
+	});
+
+	app.get("/items/:id", (request, response) => {
+		pool.query("SELECT * FROM items WHERE id = ?", request.params.id, (error, result) => {
+			if(error){
+				return console.log(error);
+			}
+			response.send(result);
+		});
+	});
+
+	app.post("/items", (request, response) => {
+		pool.query("INSERT INTO items SET ?", request.body, (error, result) => {
+			if(error){
+				return console.log(error);
+			}
+			response.status(201).send("Item added with ID : " + result.insertId + "\n");
+		});
+	});
+
+	app.put("/items/:id", (request, response) => {
+		pool.query("UPDATE items SET ? WHERE id = ?", [request.body, request.params.id], (error, result) => {
+			if(error){
+				return console.log(error);
+			}
+			response.send("Item updated successfully!\n");
+		});
+	});
+
+	app.delete("/items/:id", (request, response) => {
+		pool.query("DELETE FROM items WHERE id = ?", request.params.id, (error, result) => {
+			if(error){
+				return console.log(error);
+			}
+			response.send("Item deleted!\n");
+		});
+	});
+
+	app.get("/orders", (request, response) => {
+		pool.query("SELECT * FROM orders", (error, result) => {
+			if(error){
+				return console.log(error);
+			}
+			response.send(result);
+		});
+	});
+
+	app.get("/orders/:userId", (request, response) => {
+		pool.query("SELECT * FROM orders WHERE userId = ?", request.params.userId, (error, result) => {
+			if(error){
+				return console.log(error);
+			}
+			response.send(result);
+		});
+	});
+
+	app.post("/orders", (request, response) => {
+		pool.query("INSERT INTO orders SET ?, time = ?", [request.body, new Date()], (error, result) => {
+			if(error){
+				return console.log(error);
+			}
+			response.status(201).send("Order added with ID : " + result.insertId + "\n");
+		});
+	});
 }
 
 module.exports = router;
