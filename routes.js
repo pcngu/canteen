f76987470edf8ec33af4285698dcfc4b24c73753
Pradeep <pcngu1@gmail.com>
2018-11-09 18:58:47 +0530

    Added cart

--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 pool = require("./config");
+req = require("request");
 
 router = app => {
 	app.get("/", (request, response) => {
@@ -104,6 +105,91 @@ router = app => {
 		else{
 			response.redirect("/login");
 		}
+	});
+
+	app.post("/menu", (request, response) => {
+		if(request.body.quantity > 0){
+			pool.query("SELECT id FROM users WHERE phone = ?", request.session.phone, (error, result) => {
+				if(result && result.length == 1){
+					req.post("http://localhost:8080/orders", {
+						json : {
+							userId : result[0]["id"],
+							itemId : request.body.itemId,
+							quantity : request.body.quantity
+						}
+					}, (error, resp, body) => {
+						if(!error && resp.statusCode == 201){
+							console.log(body);
+						}
+					});
+				}
+				else{
+					request.session.destroy((error) => {
+						if(error){
+							return console.log(error);
+						}
+						response.render("login", {
+							showError : 1,
+							loginError : "Please login again!"
+						});
+					});
+				}
+			});
+		}
+	});
+
+	app.get("/cart", (request, response) => {
+		if(request.session.phone){
+			pool.query("SELECT orders.id, orders.quantity, items.name, items.image FROM items INNER JOIN orders ON orders.active = 0 AND orders.itemId = items.id INNER JOIN users ON orders.userId = users.id AND users.phone = ?", request.session.phone, (error, result) => {
+				if(error){
+					return console.log(error);
+				}
+				response.render("cart", {
+					orders : result,
+				});
+			});
+		}
+		else{
+			response.redirect("/login");
+		}
+	});
+
+	app.put("/cart", (request, response) => {
+		pool.query("SELECT id FROM users WHERE phone = ?", request.session.phone, (error, result) => {
+			if(result && result.length == 1){
+				req.put("http://localhost:8080/orders/"+result[0]["id"], {
+					json : {
+						active : 1
+					}
+				}, (error, resp, body) => {
+					if(!error && resp.statusCode == 200){
+						console.log(body);
+					}
+				});
+			}
+			else{
+				request.session.destroy((error) => {
+					if(error){
+						return console.log(error);
+					}
+					response.render("login", {
+						showError : 1,
+						loginError : "Please login again!"
+					});
+				});
+			}
+		});
+	});
+
+	app.delete("/cart/:id", (request, response) => {
+		req.delete("http://localhost:8080/orders/"+request.params.id, {
+			json : {
+			}
+		}, (error, resp, body) => {
+			if(!error && resp.statusCode == 200){
+				console.log(body);
+			}
+		});
 	});
 
 	app.get("/users", (request, response) => {
@@ -216,37 +302,20 @@ router = app => {
 	});
 
 	app.post("/orders", (request, response) => {
-		if(request.body.quantity > 0){
-			pool.query("SELECT id FROM users WHERE phone = ?", request.session.phone, (error, resultU) => {
-				if(resultU && resultU.length == 1){
-					pool.query("INSERT INTO orders SET ?, userId = ?, time = ?", [request.body, resultU[0]["id"], new Date()], (error, resultO) => {
-						if(error){
-							return console.log(error);
-						}
-						response.status(201).send("Order added with ID : " + resultO.insertId + "\n");
-					});
-				}
-				else{
-					request.session.destroy((error) => {
-						if(error){
-							return console.log(error);
-						}
-						response.render("login", {
-							showError : 1,
-							loginError : "Please login again!"
-						});
-					});
-				}
-			});
-		}
-	});
-
-	app.put("/orders/:id", (request, response) => {
-		pool.query("UPDATE orders SET ? WHERE id = ?", [request.body, request.params.id], (error, result) => {
+		pool.query("INSERT INTO orders SET ?, time = ?", [request.body, new Date()], (error, result) => {
 			if(error){
 				return console.log(error);
 			}
-			response.send("Order updated successfully!\n");
+			response.status(201).send("Order added with ID : " + result.insertId + "\n");
+		});
+	});
+
+	app.put("/orders/:userId", (request, response) => {
+		pool.query("UPDATE orders SET ? WHERE userId = ?", [request.body, request.params.userId], (error, result) => {
+			if(error){
+				return console.log(error);
+			}
+			response.send("Orders updated successfully!\n");
 		});
 	});
 
